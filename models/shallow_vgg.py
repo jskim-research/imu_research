c04ec2606bebae6bb9ec785cd6c4775a82e377de
jskim-research <wltnwkd21@gmail.com>
2022-07-11 18:45:40 +0900

    Fix: Grad CAM 반영을 위한 수정

--- models/shallow_vgg.py
+++ models/shallow_vgg.py
@@ -5,6 +5,33 @@ from tensorflow.keras.layers import Layer
 from tensorflow.keras.models import Model
 
 
+def shallow_vgg(layer1_num: int, layer2_num: int, layer3_num: int):
+    inp = layers.Input(shape=(128, 32, 24))
+    out = inp
+
+    for _ in range(layer1_num):
+        out = layers.Conv2D(filters=64, kernel_size=(3, 3), strides=1, padding="same")(out)
+        out = layers.BatchNormalization()(out)
+        out = layers.ReLU()(out)
+    out = layers.MaxPool2D(pool_size=(2, 2), strides=2, padding="same")(out)
+
+    for _ in range(layer2_num):
+        out = layers.Conv2D(filters=128, kernel_size=(3, 3), strides=1, padding="same")(out)
+        out = layers.BatchNormalization()(out)
+        out = layers.ReLU()(out)
+    out = layers.MaxPool2D(pool_size=(2, 2), strides=2, padding="same")(out)
+
+    final_conv_output = None
+    for _ in range(layer3_num):
+        out = layers.Conv2D(filters=256, kernel_size=(3, 3), strides=1, padding="same", name="final_conv")(out)
+        final_conv_output = out
+        out = layers.BatchNormalization()(out)
+        out = layers.ReLU()(out)
+    out = layers.GlobalAvgPool2D()(out)
+    out = layers.Dense(1, name="custom_output")(out)
+    return keras.Model(inputs=[inp], outputs=[out, final_conv_output])
+
+
 class ShallowVGG(Model):
     def __init__(self, layer1_num: int, layer2_num: int, layer3_num: int):
         super().__init__()
@@ -26,8 +53,7 @@ class ShallowVGG(Model):
             self.sequence.add(layers.Conv2D(filters=256, kernel_size=(3, 3), strides=1, padding="same"))
             self.sequence.add(layers.BatchNormalization())
             self.sequence.add(layers.ReLU())
-        self.sequence.add(layers.MaxPool2D(pool_size=(2, 2), strides=2, padding="same"))
-        self.sequence.add(layers.Flatten())
+        self.sequence.add(layers.GlobalAvgPool2D())
         self.sequence.add(layers.Dense(1))
 
     def call(self, inputs, training=None, mask=None):
@@ -79,5 +105,12 @@ def CompositeVGG():
 
 if __name__ == "__main__":
     rand_input = tf.random.normal((1, 128, 32, 24))
-    model = ShallowVGG(1, 1, 1)
-    print(model(rand_input).shape)
+    # model = ShallowVGG(1, 1, 1)
+    model = shallow_vgg(1, 1, 1)
+
+    with tf.GradientTape() as tape:
+        out, final_conv_out = model(rand_input)
+
+    grad = tape.gradient(out, final_conv_out)  # d_out / d_final_conv_out
+    print(final_conv_out.shape)
+    print(grad.shape)
